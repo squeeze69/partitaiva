9e8ad16132e26286507b60fa8eb4bd5fc6e5fdfb
Marco su Acerrimo3 <mbanfi@gmail.com>
2017-05-01 16:33:41 +0200

    modified:   partitaiva.go - aggiunto test per stringhe non numeriche - corretto problema di verifica codice di controllo modified:   partitaiva_test.go aggiunti alcuni test

--- partitaiva.go
+++ partitaiva.go
@@ -1,6 +1,11 @@
 package partitaiva
 
+import "unicode"
+
 //Verifica partita IVA - 2017 - Squeeze69
+//Porting (più o meno) dalla versione in PHP presente sul sito Icosaedro:
+//un grazie ad Umberto Salsi
+//http://www.icosaedro.it/cf-pi/
 
 //PIVAError : Partita IVA - error
 type PIVAError struct {
@@ -15,14 +20,19 @@ func (r *PIVAError) Error() string {
 func ItPartitaIva(piva string) (bool, *PIVAError) {
 	ordv := map[string]int{
 		"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
-		"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7,
-		"I": 8, "J": 9, "K": 10, "L": 11, "M": 12, "N": 13, "O": 14, "P": 15, "Q": 16, "R": 17,
-		"S": 18, "T": 19, "U": 20, "V": 21, "W": 22, "X": 23, "Y": 24, "Z": 25,
 	}
 	if len(piva) != 11 {
 		er := new(PIVAError)
 		er.msg = "Wrong Length"
 		return false, er
+	}
+
+	for _, c := range piva {
+		if !unicode.IsDigit(c) {
+			er := new(PIVAError)
+			er.msg = "Non numeric"
+			return false, er
+		}
 	}
 	var primo, secondo, i int
 	for i = 0; i <= 9; i += 2 {
@@ -35,15 +45,8 @@ func ItPartitaIva(piva string) (bool, *PIVAError) {
 		}
 		primo += secondo
 	}
-	switch primo % 10 {
-	case 0:
-		if ordv[string(piva[10])] == 0 {
-			return true, nil
-		}
-	default:
-		if (10 - primo%10) == ordv[string(piva[10])] {
-			return true, nil
-		}
+	if (10-primo%10)%10 == ordv[string(piva[10])] {
+		return true, nil
 	}
 
 	er := new(PIVAError)
--- partitaiva_test.go
+++ partitaiva_test.go
@@ -6,25 +6,25 @@ import (
 )
 
 func TestItPartitaiva(t *testing.T) {
-	pivaOk := []string{"12345678903","00000000000", "44444444440"}
-	pivaKO := []string{"xxxxxxxxxxxx","123456789012","44444444444","12345678901"}
+	pivaOk := []string{"12345678903", "00000000000", "44444444440"}
+	pivaKO := []string{"xxxxxxxxxxx", "123456789012", "44444444444", "12345678901", "44444+44440", "corto"}
 	for _, v := range pivaOk {
 		res, err := ItPartitaIva(v)
 		if err != nil {
 			t.Fatal("Error, ", v, " should be valid", err)
 		}
 		if res {
-			fmt.Println("Ok (valid)",v)
+			fmt.Println("Ok (valid)", v)
 		}
 	}
 
-	for _,v := range pivaKO {
+	for _, v := range pivaKO {
 		res, err := ItPartitaIva(v)
 		if err == nil {
-			t.Fatal("Error, ", v," should be invalid")
+			t.Fatal("Error, ", v, " should be invalid")
 		}
 		if !res {
-			fmt.Println("Ok (invalid)",v, err)
+			fmt.Println("Ok (invalid)", v, err)
 		}
 	}
 }
