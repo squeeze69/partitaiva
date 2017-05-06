b8888b58fb7726ea0ef284e2ae1c22fe1df060ef
Marco su Acerrimo3 <mbanfi@gmail.com>
2017-05-06 10:05:22 +0200

    modified:   README.md inserito esempio modified:   partitaiva_test.go uniformati all'italiano i commenti ed i messaggi di test

--- README.md
+++ README.md
@@ -9,4 +9,22 @@ package: go get github.com/squeeze69/partitaiva
 Porting basato sulle informazioni pubblicate da Umberto Salsi su Icosaedro:
 sito web: http://www.icosaedro.it/cf-pi/index.html
 
+Uso:
+
+package main
+
+import (
+	"github.com/squeeze69/partitaiva"
+	"fmt"
+)
+
+func main() {
+	ok, err := partitaiva.ItPartitaIva("12334566")
+	if err != nil {
+		fmt.Println("Partita IVA non valida:",err)
+	} else {
+		fmt.Println("Partita IVA valida")
+	}
+}
+
 per il codice fiscale, vedi: https://github.com/squeeze69/codicefiscale
--- partitaiva_test.go
+++ partitaiva_test.go
@@ -6,26 +6,28 @@ import (
 )
 
 func TestItPartitaiva(t *testing.T) {
+	//partite IVA valide (formalmente)
 	pivaOk := []string{"12345678903", "00000000000", "44444444440",""}
+	//partite IVA non valide
 	pivaKO := []string{"xxxxxxxxxxx", "123456789012", "44444444444", "12345678901", "44444+44440", "corto", "123456789.3",
 	"4444X444440"}
 	for _, v := range pivaOk {
 		res, err := ItPartitaIva(v)
 		if err != nil {
-			t.Fatal("Ko. Error, ", v, " should be valid", err)
+			t.Fatal("Ko. Errore, ", v, " dovrebbe essere valido", err)
 		}
 		if res {
-			fmt.Printf("Ok (valid) \"%s\"\n", v)
+			fmt.Printf("Ok (valido) \"%s\"\n", v)
 		}
 	}
 
 	for _, v := range pivaKO {
 		res, err := ItPartitaIva(v)
 		if err == nil {
-			t.Fatal("Ko. Error, ", v, " should be invalid")
+			t.Fatal("Ko. Errore, ", v, " NON dovrebbe essere valido")
 		}
 		if !res {
-			fmt.Printf("Ok (invalid) \"%s\",%s\n", v, err)
+			fmt.Printf("Ok (non valido) \"%s\",%s\n", v, err)
 		}
 	}
 }
