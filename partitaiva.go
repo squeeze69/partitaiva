13d2151a70e8fdad1dedbb08a56b3236e83096bc
Marco su Acerrimo3 <mbanfi@gmail.com>
2017-05-28 19:28:39 +0200

    modified:   partitaiva.go un solo ciclo per il calcolo del codice di controllo funzione interna per genera *PIVAError

--- partitaiva.go
+++ partitaiva.go
@@ -24,36 +24,35 @@ func (r *PIVAError) Error() string {
 	return r.msg
 }
 
+//errPIVA : genera un *PIVAError, non esportata
+func errPIVA(s string) *PIVAError {
+	err := new(PIVAError)
+	err.msg = s
+	return err
+}
+
 //ItPartitaIva controlla se è una partita IVA valida nel formato
 //Nota: se piva è vuota, viene considerata valida, per questo caso, il controllo dovrebbe essere altrove
 //Ingresso: piva: stringa
 //Uscita: bool:true (a posto)/false (problemi) e *PIVAError (nil (a posto)/puntatore all'errore (problemi)
 func ItPartitaIva(piva string) (bool, *PIVAError) {
-	if len(piva) == 0 {
-		return true,nil
+	if len(piva) == 0 {	//convenzione da Icosaedro
+		return true, nil
 	}
 	if len(piva) != 11 {
-		er := new(PIVAError)
-		er.msg = "Lunghezza Sbagliata"
-		return false, er
+		return false, errPIVA("Lunghezza Sbagliata")
 	}
 
 	var primo, secondo, i int
 	for i = 0; i <= 9; i += 2 {
-		v,err := strconv.Atoi(string(piva[i]))
+		v, err := strconv.Atoi(string(piva[i]))
 		if err != nil {
-			er := new(PIVAError)
-			er.msg = "Caratteri Non Validi"
-			return false, er
+			return false, errPIVA("Caratteri Non Validi")
 		}
 		primo += v
-	}
-	for i = 1; i <= 9; i += 2 {
-		v,err := strconv.Atoi(string(piva[i]))
+		v, err = strconv.Atoi(string(piva[i+1]))
 		if err != nil {
-			er := new(PIVAError)
-			er.msg = "Caratteri Non Validi"
-			return false, er
+			return false, errPIVA("Caratteri Non Validi")
 		}
 		secondo = 2 * v
 		if secondo > 9 {
@@ -61,12 +60,10 @@ func ItPartitaIva(piva string) (bool, *PIVAError) {
 		}
 		primo += secondo
 	}
-	if v,_ := strconv.Atoi(string(piva[10])); (10-primo%10)%10 == v {
+	//corrisponde?
+	if v, _ := strconv.Atoi(string(piva[10])); (10-primo%10)%10 == v {
 		return true, nil
 	}
-
-	er := new(PIVAError)
-	er.msg = "Carattere Di Controllo Non Valido"
-	return false, er
-
+	//non corrisponde
+	return false, errPIVA("Carattere Di Controllo Non Valido")
 }
